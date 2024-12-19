802e37d3feba5d1eac8ab3a9a9efd1b923015bd0
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-19 17:56:31 +0700

    imp signup controller

--- src/main/java/com/aaron/osahaneat/controller/LoginController.java
+++ src/main/java/com/aaron/osahaneat/controller/LoginController.java
@@ -1,15 +1,13 @@
 package com.aaron.osahaneat.controller;
 
 import com.aaron.osahaneat.payload.ResponseData;
+import com.aaron.osahaneat.payload.request.SignupRequest;
 import com.aaron.osahaneat.service.imp.LoginServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
@@ -20,7 +18,7 @@ public class LoginController {
     private LoginServiceImp loginService;
 
     @PostMapping("/signin")
-    public ResponseEntity<?> login(@RequestParam String userName, @RequestParam String password) {
+    public ResponseEntity<?> signin(@RequestParam String userName, @RequestParam String password) {
         ResponseData responseData = new ResponseData();
         if(this.loginService.checkLogin(userName, password) == true) {
             responseData.setStatus(HttpStatus.OK.value());
@@ -29,6 +27,15 @@ public class LoginController {
         }
 
         return new ResponseEntity(responseData, HttpStatus.OK);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest) {
+        ResponseData responseData = new ResponseData();
+
+        responseData.setData(this.loginService.addUser(signupRequest));
+
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
 
 }
